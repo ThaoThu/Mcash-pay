da78ebce9de3f52e3fce51b776192e5ec66c0c67
Thao Thu Nguyen <thaothung.04@gmail.com>
2019-06-05 14:41:59 +0700

    v4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,77 +7,76 @@ export default class Footer extends Component {
   render() {
     return (
       <footer style={{backgroundImage:`url(${ftBg})`}}>
-            <div className="primary-logo">
-                <p className="text text-center">2013-2019 Coinpayments. Inc. All rights Reserved. Wallet Services provided by Hodtech OU licence number FH435</p>
-                <div className="footer-div ">
-                    <div className="container">
-                        <div className="row">
-                            <div className = "col-4 col-lg-2 ">
-                                <div className ="footer-link">
-                                    <h5 className = "footer-link__title">COINT PAYMENT</h5>
-                                    <ul>
-                                        <li><a>Fees/Pricing</a></li>
-                                        <li><a>Supperied Coints</a></li>
-                                        
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className = "col-4 col-lg-2">
-                                <div className ="footer-link">
-                                    <h5 className = "footer-link__title">RESOURCES</h5>
-                                    <ul>
-                                        <li><a>Merchant Tools </a></li>
-                                        <li><a>Affinate Information </a></li>
-                                        <li><a>FAQ</a></li>
-                                        <li><a>Offical TORIRI</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className = "col-4 col-lg-2">
-                                <div className ="footer-link">
-                                    <h5 className = "footer-link__title">CONTACT</h5>
-                                    <ul>
-                                        <li><a>Add your coin</a></li>
-                                        <li><a>Add your store </a></li>
-                                        <li><a>Support</a></li>
-                                        <li><a>Contact us</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className = "col-4 col-lg-2">
-                                <div className ="footer-link">
-                                    <h5 className = "footer-link__title">SOCIAL</h5>
-                                    <ul>
-                                        <li><a>Blog</a></li>
-                                        <li><a>Twitter</a></li>
-                                        <li><a>Instagram</a></li>
-                                        <li><a>Telegram</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className = "col-4 col-lg-2">
-                                <div className ="footer-link">
-                                    <h5 className = "footer-link__title">LEGAL</h5>
-                                    <ul>
-                                        <li><a>Primary Policy</a></li>
-                                        <li><a>User Agreement</a></li>
-                                        <li><a>GDPR Notice</a></li>
-                                        
-                                    </ul>
-                                </div>
-                            </div>
+        <p className="text text-center">2013-2019 Coinpayments. Inc. All rights Reserved. Wallet Services provided by Hodtech OU licence number FH435</p>
+        <div className="footer-div ">
+            <div className="container">
+                <div className="row">
+                    <div className = "col-4 col-lg-2 ">
+                        <div className ="footer-link">
+                            <h5 className = "footer-link__title">COINT PAYMENT</h5>
+                            <ul>
+                                <li><a>Fees/Pricing</a></li>
+                                <li><a>Supperied Coints</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div className = "col-4 col-lg-2">
+                        <div className ="footer-link">
+                            <h5 className = "footer-link__title">RESOURCES</h5>
+                            <ul>
+                                <li><a>Merchant Tools </a></li>
+                                <li><a>Affinate Information </a></li>
+                                <li><a>FAQ</a></li>
+                                <li><a>Offical TORIRI</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className = "col-4 col-lg-2">
+                        <div className ="footer-link">
+                            <h5 className = "footer-link__title">CONTACT</h5>
+                            <ul>
+                                <li><a>Add your coin</a></li>
+                                <li><a>Add your store </a></li>
+                                <li><a>Support</a></li>
+                                <li><a>Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className = "col-4 col-lg-2">
+                        <div className ="footer-link">
+                            <h5 className = "footer-link__title">SOCIAL</h5>
+                            <ul>
+                                <li><a>Blog</a></li>
+                                <li><a>Twitter</a></li>
+                                <li><a>Instagram</a></li>
+                                <li><a>Telegram</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className = " col-4 col-lg-2">
+                        <div className ="footer-link">
+                            <h5 className = "footer-link__title">LEGAL</h5>
+                            <ul>
+                                <li><a>Primary Policy</a></li>
+                                <li><a>User Agreement</a></li>
+                                <li><a>GDPR Notice</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
 
-                            <div className = "col-4 col-lg-2">
-                                <div className ="footer-link">
-                                    <button className="btn btn-footer text-white mb-2">Add your coin</button>
-                                    <img src={appStoreImg } className=" mb-2 img-fluid"></img>
-                                    <img src={ggStoreImg} className="img-fluid"></img>
-                                </div>
-                            </div>
+                    <div className = "col-4 col-lg-2">
+                        <div className ="footer-link footer-img">
+                            <button className="btn btn-footer text-white mb-2">Add your coin</button>
+                            <img src={appStoreImg } className=" mb-2 img-fluid"></img>
+                            <img src={ggStoreImg} className="mb-2 img-fluid"></img>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    
 
       
       </footer>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,27 +8,23 @@ export default class Nav extends Component {
     return (
     <div  className ="header-nav">
         <div className="container">
-            <div className="row " style={{position:'relative'}}>
-                <nav style={{position:'absolute'}} className="navbar navbar-expand-lg  navbar-light   col-12">
+            <div className="row " style={{position:'relative',zIndex:'9999'}}>
+                <nav style={{position:'absolute'}} className="navbar navbar-expand-xl  navbar-light   col-12">
                     <a className="navbar-brand" href="#"><img className="img-fluid" src={logo}></img></a>
                     <button className="navbar-toggler d-xl-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon">
                             <Icon type="menu" style={{color:'#fff'}}/>
-                            
                         </span>
                     </button>
                     <div className="collapse navbar-collapse" id="collapsibleNavId">
                         <span className="form-inline my-2 my-lg-0 ml-auto">
                         <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                             <li className="nav-item ">
-                                <a className="nav-link" href="#">Merchant Tools<span className="sr-only">(current)</span></a>
+                                <a className="nav-link" href="#">Dashboard<span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Add your coin</a>
-                            </li>
-                            <li className="nav-item ">
-                                <a className="nav-link" href="#">Contacts<span className="sr-only">(current)</span></a>
+                                <a className="nav-link" href="#">Supported Coins</a>
                             </li>
                             <li className="nav-item ">
                                 <a className="nav-link border-item" href="#">Login</a>
@@ -36,12 +32,23 @@ export default class Nav extends Component {
                             <li className="nav-item ">
                                 <a className="nav-link border-item" href="#">Sign up</a>
                             </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Contacts<span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Transactions<span className="sr-only">(current)</span></a>
+                            </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language</a>
+                                <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
                                 <div className="dropdown-menu" aria-labelledby="dropdownId">
-                                    <a className="dropdown-item" href="#">Vi</a>
-                                    <a className="dropdown-item" href="#">En</a>
+                                    <a className="dropdown-item" href="#">My wallet</a>
+                                    <a className="dropdown-item" href="#">Api Key</a>
+                                    <a className="dropdown-item" href="#">Security Setting</a>
+                                    <a className="dropdown-item" href="#">Funds</a>
                                 </div>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Sign out<span className="sr-only">(current)</span></a>
                             </li>
                         </ul>
                             
